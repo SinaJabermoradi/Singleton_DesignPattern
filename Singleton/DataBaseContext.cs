ebafe990497db65ce367fd6dee8dbd29b6b59477
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-07 20:26:23 +0430

    Create DataBaseContext

---
+++ Singleton/DataBaseContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class DataBaseContext<T> : IDataBaseContext<T>
+    {
+        private static DataBaseContext<T> _context;
+
+
+        public static DataBaseContext<T> GetContext
+        {
+            get
+            {
+                _context = new DataBaseContext<T>();
+
+                return _context;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Singleton/IDataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+   public interface IDataBaseContext<T>
+    {
+        public static DataBaseContext<T> GetContext { get; }
+
+    }
+}
