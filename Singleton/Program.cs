6c4a92bf2ae7ec7c1f06988624ed622c25eadc49
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-07 20:56:26 +0430

    Finish Program

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -6,7 +6,29 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DataBaseContext<IData>.GetContext.Add(new Data
+            {
+                Name = "sina",
+                Family = "Jabermoradi",
+                Age = 21,
+                Id = 1
+            });
+
+            DataBaseContext<IData>.GetContext.Delete(new Data
+            {
+                Name = "Samane",
+                Family = "Sasani",
+                Age = 25,
+                Id = 2
+            });
+            
+            DataBaseContext<IData>.GetContext.Update(5,new Data
+            {
+                Name = "sina",
+                Family = "falaki",
+                Age = 21,
+                Id = 3
+            });
         }
     }
 }
