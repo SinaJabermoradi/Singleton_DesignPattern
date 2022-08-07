bb7638bfb639feba448c30a0f698d0fe83ca3a62
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-07 20:33:43 +0430

    Update Context

--- Singleton/DataBaseContext.cs
+++ Singleton/DataBaseContext.cs
@@ -6,21 +6,58 @@ namespace Singleton
 {
     public class DataBaseContext<T> : IDataBaseContext<T>
     {
+
         private static DataBaseContext<T> _context;
 
 
-        public static DataBaseContext<T> GetContext
-        {
-            get
-            {
-                _context = new DataBaseContext<T>();
+        #region Behaviors
 
-                return _context;
-            }
+        #region Constructors
+
+        private DataBaseContext()
+        {
+
+
         }
 
+    #endregion
+
+      
+        #region Getter & Setter
 
 
+    public static DataBaseContext<T> GetContext
+    {
+        get
+        {
+            _context = new DataBaseContext<T>();
 
+            return _context;
+        }
+    }
+
+
+        #endregion
+
+
+        #region Opereations
+
+        public void Add(T data)
+        {
+            Console.WriteLine("Add To DataBase ");
+        }
+        public void Delete(T data)
+        {
+            Console.WriteLine("Delete To DataBase ");
+        }
+        public void Update(long dataId,T data)
+        {
+            Console.WriteLine("Update To DataBase ");
+        }
+        
+
+        #endregion
+
+        #endregion
     }
 }
